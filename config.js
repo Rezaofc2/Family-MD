7c8b6b1f8971d1ed0ebcb151d1e3e6cc74ae8753
Rezaofc4 <106759741+Rezaofc4@users.noreply.github.com>
2022-06-17 09:25:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,11 +43,11 @@ global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
 
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/aguzfamilia"
+global.urlnya = "https://www.instagram.com/ezaofc"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 896-0679-0112'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
